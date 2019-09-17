aea4a7f9c6aff1ef1ab20069fa791dbb56756e7d
ditiashova <inna.ditiashova@gmail.com>
2019-09-17 21:23:03 +0300

    gitgraphjs sandbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
+import { createGitgraph } from "@gitgraph/js";
+import { GitgraphUserApi } from '@gitgraph/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  @ViewChild('gitgraph', {static: true}) gitgraphEl: ElementRef;
   title = 'life-init';
+  gitgraph: GitgraphUserApi<any>;
+
+  ngAfterViewInit() {
+    this.gitgraph = createGitgraph(this.gitgraphEl.nativeElement);
+    const master = this.gitgraph.branch('master');
+    master.commit("Init the project");
+    master
+        .commit("Add README")
+        .commit("Add tests")
+        .commit("Implement feature");
+
+
+  }
 }
